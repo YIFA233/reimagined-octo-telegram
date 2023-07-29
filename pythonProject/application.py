b72c5cdf618c80227e9cf9257dff99e44b96c25b
YIFA233 <112579794+YIFA233@users.noreply.github.com>
2023-07-29 14:44:28 +0800

    Add files via upload

---
+++ pythonProject/application.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = joblib.load('model.pkl')
+
+    data = {
+        'Pregnancies': float(request.form.get('Pregnancies')),
+        'Glucose': float(request.form.get('Glucose')),
+        'BloodPressure': float(request.form.get('BloodPressure')),
+        'SkinThickness': float(request.form.get('SkinThickness')),
+        'Insulin': float(request.form.get('Insulin')),
+        'BMI': float(request.form.get('BMI')),
+        'DiabetesPedigreeFunction': float(request.form.get('DiabetesPedigreeFunction')),
+        'Age': float(request.form.get('Age'))
+    }
+
+    features = np.array([list(data.values())])
+    prediction_proba = model.predict_proba(features)[0]
+
+    return f"The probability of the patient having diabetes is {prediction_proba[1]*100:.2f}%."
+
+if __name__ == '__main__':
+    app.run(debug=True)
